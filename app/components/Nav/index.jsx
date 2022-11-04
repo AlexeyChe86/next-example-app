e87e3795313edac0cad3387cda0d4c5ffafb147c
alexey.cherebayev <alexey.cherebayev@telesy.kz>
2022-11-04 20:13:10 +0600

    Changed style nav links

--- app/components/Nav/index.jsx
+++ app/components/Nav/index.jsx
@@ -1,16 +1,17 @@
 'use client'
 
 import Link from 'next/link'
-
+import { usePathname } from 'next/navigation'
 import menu from '../../helpers/nav/config'
 
 export default function index() {
+  const pathName = usePathname()
   return (
-    <nav className='flex bg-blue-700 text-white p-5'>
+    <nav className='flex bg-blue-700 text-white'>
       {menu.map((item) => (
         <div
           key={item.id}
-          className='mr-4'
+          className={item.path === pathName ? 'bg-yellow-400 p-5 mx-1' : 'p-5 mx-1'}
         >
           <Link href={item.path}>{item.name}</Link>
         </div>
