22a961cfae1f4714c954d1833c76f27518cc0e65
alexey.cherebayev <alexey.cherebayev@telesy.kz>
2022-11-04 19:45:44 +0600

    Added nav config

--- app/components/Nav/index.jsx
+++ app/components/Nav/index.jsx
@@ -2,15 +2,19 @@
 
 import Link from 'next/link'
 
+import menu from '../../helpers/nav/config'
+
 export default function index() {
   return (
     <nav className='flex bg-blue-700 text-white p-5'>
-      <div>
-        <Link href='/'>Home</Link>
-      </div>
-      <div className='ml-4'>
-        <Link href='/posts'>Post</Link>
-      </div>
+      {menu.map((item) => (
+        <div
+          key={item.id}
+          className='mr-4'
+        >
+          <Link href={item.path}>{item.name}</Link>
+        </div>
+      ))}
     </nav>
   )
 }
---
+++ app/helpers/nav/config.ts
@@ -0,0 +1,14 @@
+const menu = [
+  {
+    id: 0,
+    name: 'Home',
+    path: '/',
+  },
+  {
+    id: 1,
+    name: 'Posts',
+    path: '/posts',
+  },
+]
+
+export default menu
